127f2535aa459aab5bd175fffb83e37e0a456cbd
DenisMekh05 <120127947+DenisMekh05@users.noreply.github.com>
2023-02-08 20:50:36 +0400

    Add files via upload

---
+++ lessons/13/js/test.js
@@ -0,0 +1,12 @@
+//1
+let num1 = -4
+num1 = Math.abs(-4)
+//2
+let num2 = 27
+num2 = num2.Math.cbrt(27)
+//3
+let num3 = 5.1
+num3 = num3.Math.ceil(5.1)
+//4
+let num4 = 5.9
+num4 = num4.Math.floor(5.9)
