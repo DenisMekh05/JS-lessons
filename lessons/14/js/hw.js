3aef4b18b3b87b1a19a42188e7caf3bb2ab1896f
DenisMekh05 <120127947+DenisMekh05@users.noreply.github.com>
2023-02-12 15:28:21 +0400

    Add files via upload

---
+++ lessons/14/js/hw.js
@@ -0,0 +1,34 @@
+function isSameNum(num1, num2) {
+    if (num1 === num2) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function and(param1, param2) {
+    if(param1 && param2) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function boolToString(param) {
+    return '"' + param + '"'
+}
+
+function convert(num1, num2) {
+    return (num1 * 3600) + (num2 * 60)
+}
+
+function isSeven(num) {
+    if(num === 7) {
+        return true
+    }
+    else {
+        return false
+    }
+}
